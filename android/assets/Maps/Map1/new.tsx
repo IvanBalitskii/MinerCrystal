3e7ceca05e6f85ab8afdae903be5256af242bd39
Ivan Balitsky <hoobchik@gmail.com>
2019-06-18 12:05:02 +0300

    CreTE

---
+++ android/assets/Maps/Map1/new.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="new" tilewidth="64" tileheight="64" tilecount="368" columns="16">
+ <image source="mapLevel12.png" width="1080" height="1500"/>
+</tileset>
